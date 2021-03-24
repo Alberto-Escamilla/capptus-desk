3b37f99ed996fb386801a901867a9afe7d4ceac3
Alberto-Escamilla <l161130082@gamadero.tecnm.mx>
2021-03-24 08:49:44 -0600

    script brands

--- js/script.js
+++ js/script.js
@@ -8,13 +8,11 @@ $(document).ready(function() {
     $('.customer-logos').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 1500,
-        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
         dots: false,
         pauseOnHover: false,
-        prevArrow: '<i class="slick-prev fas fa-angle-left"></i>',
-        nextArrow: '<i class="slick-next fas fa-angle-right"></i>',
         responsive: [{
             breakpoint: 768,
             settings: {
